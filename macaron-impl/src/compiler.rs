af8668b8bf5876ade74be2b24cc0b84abdfdf94f
jjl <james.laver@gmail.com>
2021-04-16 10:29:57 +0200

    first static check - pubness matches

--- macaron-impl/src/compiler.rs
+++ macaron-impl/src/compiler.rs
@@ -1,5 +1,6 @@
 use crate::ast::*;
 use crate::diag::*;
+use proc_macro2::Span;
 use std::collections::HashMap;
 
 #[derive(Default)]
@@ -20,13 +21,44 @@ pub struct Macaron {
 impl Macaron {
     pub fn check(&self) -> Result<(), Vec<Diagnostic>> {
         let mut diag = Vec::new();
-        for rule in self.rules.iter() {
-            self.validate_rule(rule, &mut diag);
+        let mut rules = self.rules.iter();
+        if let Some(first) = rules.next() {
+            let mut pub_spans = vec!();
+            if let Some(pub_token) = first.pub_token {
+                for rule in rules {
+                    if !rule.pub_token.is_some() {
+                        pub_spans.push(rule.macro_token.span);
+                    }
+                }
+                if !pub_spans.is_empty() {
+                    diag.push(Diagnostic::spanned(
+                        pub_spans, Level::Error, "Missing pub"
+                    ).span_note(
+                        pub_token.span, "Original (pub) macro definition here"
+                    ));
+                }
+            } else {
+                for rule in rules {
+                    if let Some(pub_token) = rule.pub_token {
+                        pub_spans.push(pub_token.span);
+                    }
+                }
+                if !pub_spans.is_empty() {
+                    diag.push(Diagnostic::spanned(
+                        pub_spans, Level::Error, "Unexpected pub"
+                    ).span_note(
+                        first.macro_token.span, "Original (non-pub) macro definition here"
+                    ));
+                }
+            }
+            if diag.is_empty() {
+                Ok(())
+            } else {
+                Err(diag)
+            }
+        } else {
+            Ok(())
         }
-        Ok(())
-    }
-    pub fn validate_rule(&self, rule: &Rule, diag: &mut Vec<Diagnostic>) {
-    
     }
 }
 
